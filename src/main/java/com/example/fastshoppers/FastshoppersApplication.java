627e7846973a12eba5b1a7e68de6e0be3b1cd0c2
takoyummy <takoyummy@naver.com>
2023-10-01 17:57:15 +0900

    init : Initial environment setup

---
+++ src/main/java/com/example/fastshoppers/FastshoppersApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastshoppers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastshoppersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastshoppersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fastshoppers/FastshoppersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastshoppers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastshoppersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
