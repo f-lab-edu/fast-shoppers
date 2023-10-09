f10781a267a655aa8837d629a4710c19c0a716fb
takoyummy <takoyummy@naver.com>
2023-10-09 16:41:20 +0900

    feat : Rename Package

--- src/main/java/com/fastshoppers/FastshoppersApplication.java
+++ src/main/java/com/fastshoppers/FastshoppersApplication.java
@@ -1,4 +1,4 @@
-package com.example.fastshoppers;
+package com.fastshoppers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/fastshoppers/FastshoppersApplicationTests.java
+++ src/test/java/com/fastshoppers/FastshoppersApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.fastshoppers;
+package com.fastshoppers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
