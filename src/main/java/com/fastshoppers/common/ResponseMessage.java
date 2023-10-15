7396eb29a1b5aae6b3054d0dd49abc011e2b04ae
takoyummy <takoyummy@naver.com>
2023-10-15 19:50:33 +0900

    feat : Update statusCode to ResponseMessage
    1. statusCode 변수를 ResponseMessage클래스에 추가하였습니다.

--- src/main/java/com/fastshoppers/common/ResponseMessage.java
+++ src/main/java/com/fastshoppers/common/ResponseMessage.java
@@ -6,17 +6,34 @@ import lombok.Data;
 public class ResponseMessage {
     private String message;
     private int status;
+    private String statusCode;
     private Object data;
 
     public ResponseMessage(String message, int status) {
         this.message = message;
         this.status = status;
+        this.statusCode = null;
+        this.data = null;
+    }
+
+    public ResponseMessage(String message, int status, String statusCode) {
+        this.message = message;
+        this.status = status;
+        this.statusCode = statusCode;
         this.data = null;
     }
 
     public ResponseMessage(String message, int status, Object data) {
         this.message = message;
         this.status = status;
+        this.statusCode = null;
+        this.data = data;
+    }
+
+    public ResponseMessage(String message, int status, String statusCode, Object data) {
+        this.message = message;
+        this.status = status;
+        this.statusCode = statusCode;
         this.data = data;
     }
 }
