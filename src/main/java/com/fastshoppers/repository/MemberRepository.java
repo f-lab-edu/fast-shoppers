9ed45f50c1e7bd127c12edc8646cae16e9ce34d5
takoyummy <takoyummy@naver.com>
2023-11-19 00:27:37 +0900

    feat: Add findByEmail at MemberRepository

--- src/main/java/com/fastshoppers/repository/MemberRepository.java
+++ src/main/java/com/fastshoppers/repository/MemberRepository.java
@@ -1,7 +1,15 @@
 package com.fastshoppers.repository;
 
+import com.fastshoppers.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    /**
+     * description : 이메일 조회
+     * @param email
+     * @return Member
+     */
+    Member findByEmail(String email);
 }
