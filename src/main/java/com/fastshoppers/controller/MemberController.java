4fc17d0c9ce27b78d22b049503afd34374f7cf08
takoyummy <takoyummy@naver.com>
2023-11-19 00:29:37 +0900

    feat : Add registerMember Mapping at MemberController

--- src/main/java/com/fastshoppers/controller/MemberController.java
+++ src/main/java/com/fastshoppers/controller/MemberController.java
@@ -1,9 +1,37 @@
 package com.fastshoppers.controller;
 
-import org.springframework.stereotype.Controller;
+import com.fastshoppers.common.ResponseMessage;
+import com.fastshoppers.common.StatusCode;
+import com.fastshoppers.entity.Member;
+import com.fastshoppers.model.MemberDto;
+import com.fastshoppers.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/members")
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public ResponseMessage registerMember(@RequestBody MemberDto memberDto) {
+
+        Member registeredMember = memberService.registerMember(memberDto);
+
+        return ResponseMessage.builder()
+                .status(HttpStatus.OK.value())
+                .statusCode(StatusCode.OK.getCode())
+                .build();
+    }
 
 }
