e05815963e1bac58edaa6da76c219a0991caca25
takoyummy <takoyummy@naver.com>
2023-11-19 00:31:06 +0900

    test: Add tests for MemberRepository

---
+++ src/test/java/com/fastshoppers/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.fastshoppers.repository;
+
+import com.fastshoppers.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void testSaveMember() {
+        // Given
+        Member member = new Member();
+        member.setEmail("test1234@google.com");
+        member.setPassword("test123456");
+
+        // When
+        Member savedMember = memberRepository.save(member);
+
+        // Then
+        assertNotNull(savedMember.getMemberNum());
+        assertEquals("test1234@google.com", savedMember.getEmail());
+    }
+}
