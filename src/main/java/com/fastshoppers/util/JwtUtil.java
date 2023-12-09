72ff31ec8d80e5e17411aa430fff28dae6a474fe
takoyummy <takoyummy@naver.com>
2023-12-10 04:15:23 +0900

    feat : Add token expiration value at JwtUtil and delete member business logic
    1. accessToken, refreshToken 유효기간을 @Value어노테이션을 통해 환경 변수로 가져왔습니다.
    2. 멤버 비즈니스 로직을 분리 및 삭제하였습니다.
    3. 네이버 코드 포맷터를 적용하였습니다.

--- src/main/java/com/fastshoppers/util/JwtUtil.java
+++ src/main/java/com/fastshoppers/util/JwtUtil.java
@@ -1,108 +1,87 @@
 package com.fastshoppers.util;
 
-import com.fastshoppers.exception.MemberNotFoundException;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtUtil {
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+	@Value("${jwt.secret}")
+	private String secretKey;
 
+	@Value("${token.access.expiry.milliseconds}")
+	private long accessTokenExpiryMilliSeconds;
 
-    /**
-     * description : 1시간의 만료기간이 있는 토큰을 생성해서 리턴
-     * @param email
-     * @return Jwt accessToken
-     */
-    public String generateAccessToken(String email) {
-        Claims claims = Jwts.claims();
-        claims.put("email", email);
+	@Value("${token.refresh.expiry.milliseconds}")
+	private long refreshTokenExpiryMilliSeconds;
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 ))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
+	/**
+	 * description : 1시간의 만료기간이 있는 토큰을 생성해서 리턴
+	 * @param claimsMap
+	 * @return Jwt accessToken
+	 */
+	public String generateAccessToken(Map<String, Object> claimsMap) {
+		Claims claims = Jwts.claims(claimsMap);
 
-    /**
-     * @description : 일주일의 만료기간이 있는 refreshToken 생성
-     * @param email
-     * @return JWT refreshToken
-     */
-    public String generateRefreshToken(String email) {
-        Claims claims = Jwts.claims();
-        claims.put("email", email);
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuedAt(new Date(System.currentTimeMillis()))
+			.setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiryMilliSeconds))
+			.signWith(SignatureAlgorithm.HS256, secretKey)
+			.compact();
+	}
 
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
+	/**
+	 * @description : 일주일의 만료기간이 있는 refreshToken 생성
+	 * @param claimsMap
+	 * @return JWT refreshToken
+	 */
+	public String generateRefreshToken(Map<String, Object> claimsMap) {
+		Claims claims = Jwts.claims(claimsMap);
 
-    /**
-     * @description : 토큰에 저장된 이메일 정보 반환
-     * @param token
-     * @return email value
-     */
-    public String getEmailFromToken(String token) {
-        Claims claims = getAllClaimsFromToken(token);
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuedAt(new Date(System.currentTimeMillis()))
+			.setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiryMilliSeconds))
+			.signWith(SignatureAlgorithm.HS256, secretKey)
+			.compact();
+	}
 
-        if(claims.get("email") == null) {
-            throw new MemberNotFoundException();
-        }
-        return claims.get("email", String.class);
-    }
+	/**
+	 * @description : 토큰의 유효기간을 반환한다.
+	 * @param token
+	 * @return
+	 */
+	public Date getExpirationFromToken(String token) {
+		Claims claims = getAllClaimsFromToken(token);
 
-    /**
-     * @description : 토큰의 유효기간을 반환한다.
-     * @param token
-     * @return
-     */
-    public Date getExpirationFromToken(String token) {
-        Claims claims = getAllClaimsFromToken(token);
+		return claims.get("expiration", Date.class);
+	}
 
-        return claims.get("expiration", Date.class);
-    }
+	/**
+	 * @description : 모든 정보를 반환한다.
+	 * @param token
+	 * @return
+	 */
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+	}
 
-    /**
-     * @description : 유효한 토큰인지 확인한다.
-     * @param token
-     * @param email
-     * @return
-     */
-    public boolean validateToken(String token, String email) {
-        String emailFromToken = getEmailFromToken(token);
-        return (emailFromToken.equals(email) && !isTokenExpired(token));
-    }
-
-    /**
-     * @description : 모든 정보를 반환한다.
-     * @param token
-     * @return
-     */
-    private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
-    }
-
-    /**
-     * @description : token이 expired되었는지 확인한다.
-     * @param token
-     * @return boolean val
-     */
-    private boolean isTokenExpired(String token) {
-        Date expiration = getExpirationFromToken(token);
-        return expiration.before(new Date());
-    }
+	/**
+	 * @description : token이 expired되었는지 확인한다.
+	 * @param token
+	 * @return boolean val
+	 */
+	private boolean isTokenExpired(String token) {
+		Date expiration = getExpirationFromToken(token);
+		return expiration.before(new Date());
+	}
 
 }
