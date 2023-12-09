8ced61b218162c74e89202f837349deac9256faf
takoyummy <takoyummy@naver.com>
2023-12-10 04:23:23 +0900

    refactor : Update TimeUnit MILLISECONDS
    1. 환경변수 단일화를 위해, Seconds에서 MilliSeconds로 단위를 수정하였습니다. 2. 네이버 코드 포맷팅을 적용했습니다.

--- src/main/java/com/fastshoppers/service/RedisService.java
+++ src/main/java/com/fastshoppers/service/RedisService.java
@@ -1,34 +1,34 @@
 package com.fastshoppers.service;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 @Service
 public class RedisService {
 
-    private final StringRedisTemplate stringRedisTemplate;
+	private final StringRedisTemplate stringRedisTemplate;
 
-    @Autowired
-    public RedisService(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
+	@Autowired
+	public RedisService(StringRedisTemplate stringRedisTemplate) {
+		this.stringRedisTemplate = stringRedisTemplate;
+	}
 
-    public void saveRefreshToken(String email, String refreshToken, long expirationTime) {
-        ValueOperations<String, String> values= stringRedisTemplate.opsForValue();
-        values.set(email, refreshToken, expirationTime, TimeUnit.SECONDS);
-    }
+	public void saveRefreshToken(String email, String refreshToken, long expirationTime) {
+		ValueOperations<String, String> values = stringRedisTemplate.opsForValue();
+		values.set(email, refreshToken, expirationTime, TimeUnit.MILLISECONDS);
+	}
 
-   public String getRefreshToken(String email) {
-       ValueOperations<String, String> values= stringRedisTemplate.opsForValue();
-       return values.get(email);
-   }
+	public String getRefreshToken(String email) {
+		ValueOperations<String, String> values = stringRedisTemplate.opsForValue();
+		return values.get(email);
+	}
 
-   public void deleteRefreshToken(String email) {
-        stringRedisTemplate.delete(email);
-   }
+	public void deleteRefreshToken(String email) {
+		stringRedisTemplate.delete(email);
+	}
 
 }
