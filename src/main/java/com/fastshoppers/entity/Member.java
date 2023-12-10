e8f7a6be7bff2a21435172f0f29a5bb5d81c5ab7
takoyummy <takoyummy@naver.com>
2023-12-11 04:05:42 +0900

    feat : Add salt and builder annotation
    1. salt값을 MemberEntity에 추가하였습니다. 2. Builder 어노테이션을 추가하였습니다.

--- src/main/java/com/fastshoppers/entity/Member.java
+++ src/main/java/com/fastshoppers/entity/Member.java
@@ -1,11 +1,23 @@
 package com.fastshoppers.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDateTime;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
@@ -13,28 +25,31 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+@Builder
 @ToString
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    @Column(nullable = false, length = 255)
-    private String email;
+	@Column(nullable = false, length = 255)
+	private String email;
 
-    @Column(nullable = false, length = 255)
-    private String password;
+	@Column(nullable = false, length = 255)
+	private String password;
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+	@CreatedDate
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
 
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
 
-    @Column(name = "delete_yn", nullable = false, length = 1, columnDefinition = "char(1)")
-    private String deleteYn;
+	@Column(name = "delete_yn", nullable = false, length = 1, columnDefinition = "char(1)")
+	private String deleteYn;
 
+	@Column(nullable = false, length = 255)
+	private String salt;
 }
