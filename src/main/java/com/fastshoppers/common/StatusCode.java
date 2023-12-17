7ef6bb0c017bedc9505df7748281f380d45e8930
takoyummy <takoyummy@naver.com>
2023-12-17 18:27:32 +0900

    feat : Update StatusCode
    1. 재고 관련 statusCode를 추가하였습니다.

--- src/main/java/com/fastshoppers/common/StatusCode.java
+++ src/main/java/com/fastshoppers/common/StatusCode.java
@@ -2,21 +2,22 @@ package com.fastshoppers.common;
 
 public enum StatusCode {
 
-    OK("OK"),
-    INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR"),
-    MEMBER_NOT_FOUND("MEMBER_NOT_FOUND"),
-    MEMBER_UNAUTHORIZED("MEMBER_UNAUTHORIZED"),
-    DUPLICATE_EMAIL("DUPLICATE_EMAIL"),
-    INVALID_PASSWORD("INVALID_PASSWORD");
+	OK("OK"),
+	INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR"),
+	MEMBER_NOT_FOUND("MEMBER_NOT_FOUND"),
+	MEMBER_UNAUTHORIZED("MEMBER_UNAUTHORIZED"),
+	DUPLICATE_EMAIL("DUPLICATE_EMAIL"),
+	INVALID_PASSWORD("INVALID_PASSWORD"),
+	INVENTORY_SHORTAGE("INVENTROY_SHORTAGE"),
+	ENTITY_NOT_FOUND("ENTITY_NOT_FOUND");
 
+	private final String code;
 
-    private final String code;
+	StatusCode(String code) {
+		this.code = code;
+	}
 
-    StatusCode(String code) {
-        this.code = code;
-    }
-
-    public String getCode() {
-        return code;
-    }
+	public String getCode() {
+		return code;
+	}
 }
