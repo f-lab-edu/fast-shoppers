ad31c24dc3a4832a650689368f2541afd42f7979
takoyummy <takoyummy@naver.com>
2023-12-24 18:21:36 +0900

    refactor: Update type generic to ResponseMessage class

--- src/main/java/com/fastshoppers/common/ResponseMessage.java
+++ src/main/java/com/fastshoppers/common/ResponseMessage.java
@@ -1,29 +1,30 @@
 package com.fastshoppers.common;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
 
 @Data
 @Builder
-public class ResponseMessage {
-    private String message;
-    private int status;
-    private String statusCode;
-    private Object data;
+public class ResponseMessage<T> {
+	private String message;
+	private int status;
+	private String statusCode;
+	private T data;
 
-    public static ResponseMessage ok() {
-        return ResponseMessage.builder()
-                .status(HttpStatus.OK.value())
-                .statusCode(StatusCode.OK.getCode())
-                .build();
-    }
+	public static ResponseMessage<?> ok() {
+		return ResponseMessage.builder()
+			.status(HttpStatus.OK.value())
+			.statusCode(StatusCode.OK.getCode())
+			.build();
+	}
 
-    public static ResponseMessage ok(Object data) {
-        return ResponseMessage.builder()
-                .status(HttpStatus.OK.value())
-                .statusCode(StatusCode.OK.getCode())
-                .data(data)
-                .build();
-    }
+	public static <T> ResponseMessage<T> ok(T data) {
+		return ResponseMessage.<T>builder()
+			.status(HttpStatus.OK.value())
+			.statusCode(StatusCode.OK.getCode())
+			.data(data)
+			.build();
+	}
 }
