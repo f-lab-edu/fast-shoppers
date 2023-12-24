d46f82d9f9811ce0ed6f78e9a63a267751b79882
takoyummy <takoyummy@naver.com>
2023-12-24 19:53:11 +0900

    feat: Add ProductEntityNotFoundException

---
+++ src/main/java/com/fastshoppers/exception/ProductEntityNotFoundException.java
@@ -0,0 +1,31 @@
+package com.fastshoppers.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.fastshoppers.common.LogLevel;
+import com.fastshoppers.common.StatusCode;
+
+public class ProductEntityNotFoundException extends BaseException {
+
+	public ProductEntityNotFoundException() {
+		super("Product Entity not found.", LogLevel.ERROR);
+	}
+
+	public ProductEntityNotFoundException(String message) {
+		super("Product Entity Not Found: " + message, LogLevel.ERROR);
+	}
+
+	public ProductEntityNotFoundException(String message, LogLevel logLevel) {
+		super(message, logLevel);
+	}
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return HttpStatus.NOT_FOUND;
+	}
+
+	@Override
+	public String getExceptionCode() {
+		return StatusCode.ENTITY_NOT_FOUND.getCode();
+	}
+}
