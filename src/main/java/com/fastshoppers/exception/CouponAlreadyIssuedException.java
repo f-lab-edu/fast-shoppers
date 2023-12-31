ca8d3f958f6a8207cfb4d56a31dbe41adf1ce9f9
takoyummy <takoyummy@naver.com>
2023-12-31 20:28:57 +0900

    feat: Add CouponAlreadyIssuedException

---
+++ src/main/java/com/fastshoppers/exception/CouponAlreadyIssuedException.java
@@ -0,0 +1,30 @@
+package com.fastshoppers.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.fastshoppers.common.LogLevel;
+import com.fastshoppers.common.StatusCode;
+
+public class CouponAlreadyIssuedException extends BaseException {
+	public CouponAlreadyIssuedException() {
+		super("Coupon is Already Issued.", LogLevel.ERROR);
+	}
+
+	public CouponAlreadyIssuedException(String message) {
+		super(message, LogLevel.ERROR);
+	}
+
+	public CouponAlreadyIssuedException(String message, LogLevel logLevel) {
+		super(message, logLevel);
+	}
+
+	@Override
+	public HttpStatus getHttpStatus() {
+		return HttpStatus.CONFLICT;
+	}
+
+	@Override
+	public String getExceptionCode() {
+		return StatusCode.COUPON_ALREADY_ISSUED.getCode();
+	}
+}
