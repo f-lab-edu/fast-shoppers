5406b1b6675b15656f2fffbcc0e67152d3b7032c
takoyummy <takoyummy@naver.com>
2023-12-31 20:34:27 +0900

    feat: Add CouponController
    1. 쿠폰 재고 조회 2. 쿠폰 발급 기능을 추가하였습니다.

---
+++ src/main/java/com/fastshoppers/controller/CouponController.java
@@ -0,0 +1,51 @@
+package com.fastshoppers.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fastshoppers.common.ResponseMessage;
+import com.fastshoppers.exception.InvalidTokenException;
+import com.fastshoppers.model.CouponResponse;
+import com.fastshoppers.service.CouponService;
+import com.fastshoppers.util.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/coupon")
+public class CouponController {
+
+	private final CouponService couponService;
+
+	private final JwtUtil jwtUtil;
+
+	@GetMapping("/{couponId}")
+	public ResponseMessage getCouponStock(@PathVariable int couponId) {
+		long stock = couponService.getCouponStock(couponId);
+		return ResponseMessage.ok(stock);
+	}
+
+	@PostMapping("/{couponId}")
+	public ResponseMessage issueCouponToMember(@RequestHeader("Authorization") String accessToken,
+		@PathVariable int couponId) {
+		String token = accessToken.replace("Bearer ", "");
+
+		boolean isAccessTokenExpired = jwtUtil.isTokenExpired(token);
+
+		String email = jwtUtil.getAllClaimsFromToken(token).get("email", String.class);
+
+		if (email == null || isAccessTokenExpired) {
+			throw new InvalidTokenException();
+		}
+
+		CouponResponse couponResponse = couponService.issueCouponToMember(couponId, email);
+
+		return ResponseMessage.ok(couponResponse);
+	}
+
+}
