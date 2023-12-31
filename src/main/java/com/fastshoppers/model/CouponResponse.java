976c89431490404ed1d580a43a77f48856135ed6
takoyummy <takoyummy@naver.com>
2023-12-31 20:30:24 +0900

    feat: Add CouponResponse

---
+++ src/main/java/com/fastshoppers/model/CouponResponse.java
@@ -0,0 +1,19 @@
+package com.fastshoppers.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CouponResponse {
+	private String uuid;
+	private int remainingStock;
+}
