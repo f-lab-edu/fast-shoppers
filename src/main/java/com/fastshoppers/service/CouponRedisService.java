57fc1cf232ab132b1c15c72d414a10d7ac113405
takoyummy <takoyummy@naver.com>
2023-12-31 20:31:42 +0900

    feat: Add CouponRedisService class
    1. 디비 쿠폰 재고 조회 작업 2. redis 디비의 coupon개수 업데이트 작업등을 추가했습니다.

---
+++ src/main/java/com/fastshoppers/service/CouponRedisService.java
@@ -0,0 +1,61 @@
+package com.fastshoppers.service;
+
+import org.redisson.api.RAtomicLong;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fastshoppers.exception.CouponNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CouponRedisService {
+
+	private final RedissonClient redissonClient;
+
+	@Value("${coupon.stock.key}")
+	private String couponStockKey;
+
+	@Value("${coupon.lock.key}")
+	private String couponLockKey;
+
+	/**
+	 * @param couponId
+	 * @return
+	 * @description : redis 디비의 쿠폰 재고 조회 작업
+	 */
+	public long checkCouponStock(int couponId) {
+		String key = couponStockKey + couponId;
+		Long stockValue = redissonClient.getAtomicLong(key).get();
+
+		if (stockValue != null) {
+			return stockValue;
+		} else {
+			throw new CouponNotFoundException();
+		}
+	}
+
+	/**
+	 * @description : redis 디비의 coupon 개수 업데이트 작업
+	 * @param couponId
+	 * @return
+	 */
+	public boolean issueCoupon(int couponId) {
+		RAtomicLong stockCount = redissonClient.getAtomicLong(couponStockKey + couponId);
+		RLock lock = redissonClient.getLock(couponLockKey + couponId);
+		lock.lock();
+		try {
+			long stock = stockCount.get();
+			if (stock > 0) {
+				return stockCount.decrementAndGet() >= 0;
+			}
+			return false;
+		} finally {
+			lock.unlock();
+		}
+	}
+
+}
